495f09909f15377651aa9cd1c48588e41dd9c536
softcafecommon <softcafe.common@gmail.com>
2023-09-29 01:44:44 +0600

    ok:

--- src/Dao/ConnectionProvider.java
+++ src/Dao/ConnectionProvider.java
@@ -7,6 +7,9 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JOptionPane;
 
@@ -16,25 +19,40 @@ import javax.swing.JOptionPane;
  */
 public class ConnectionProvider {
 
-    Connection conn=null;
-    public static Connection getConnectDatabase(){
-        
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/eps","root","");
+    Connection conn = null;
+
+    public static Connection getConnectDatabase() {
+
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.sqlite.JDBC");
+//            Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/eps","root","");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:db.sqlite", "root", "root");
             JOptionPane.showMessageDialog(null, "Connection to database is successful");
-      
+            try (final Statement stmt = conn.createStatement()) {
+                stmt.execute("CREATE TABLE test (data TEXT(10));");
+//                stmt.execute("INSERT INTO test VALUES('hellooioio');");
+//                stmt.execute("INSERT INTO test VALUES('helloiiii');");
+//
+//
+//                ResultSet rs = stmt.executeQuery("SELECT * FROM test");
+//                
+//                while(rs.next()){
+//                System.out.println(rs.getString("data"));
+//                }
+            }
+
+//            conn.close();
             return conn;
-           
-            
-        }catch (Exception e){
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
             return null;
         }
-        
+
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         getConnectDatabase();
     }
-    
+
 }
